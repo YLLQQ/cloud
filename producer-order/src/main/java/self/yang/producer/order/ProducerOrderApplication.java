f7cb637ad0945afb62dad915ad84711cd31beeb2
yangguoqing <yangguoqing@zjptmc.com>
2019-08-23 11:08:40 +0800

    将用户、订单、网关模块配置前移到配置中心

--- producer-order/src/main/java/self/yang/producer/order/ProducerOrderApplication.java
+++ producer-order/src/main/java/self/yang/producer/order/ProducerOrderApplication.java
@@ -3,6 +3,11 @@ package self.yang.producer.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 订单模块
+ *
+ * @author eleven
+ */
 @SpringBootApplication
 public class ProducerOrderApplication {
 
