e2dfa2bd4a7379f12829c30422ff45fa151b8601
yangguoqing <yangguoqing@zjptmc.com>
2019-08-23 10:27:49 +0800

    增加配置中心

---
+++ config/src/main/java/self/yang/config/ConfigApplication.java
@@ -0,0 +1,20 @@
+package self.yang.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 配置中心
+ *
+ * @author eleven
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+
+}
